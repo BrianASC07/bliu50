2b81364ec0e2285031a9f082a1cb3ff91a76c81d
Brian Liu <bliu50@stuy.edu>
2024-09-16 10:59:20 -0400

    updated folders for 03 + 04

---
+++ 04_random_dict/krews.py
@@ -0,0 +1,28 @@
+#<Brian> <Liu>
+#<45>
+#SoftDev
+#K<04> -- <Python dictionairies and random selection/dictionairies and random selection/practiced using random selection on elements in a dict>
+#<2024>-<09>-<16>
+#time spent: <elapsed time in hours, rounded to nearest tenth>
+import random
+krewes = {
+           4: [ 
+        'DUA','TAWAB','EVA','JACK','VICTOR','EVAN','JASON','COLYI','IVAN','TANZEEM',
+        'TAHMIM','STANLEY','LEON','NAOMI','NIA','ANASTASIA','JADY','BRIAN','JACOB',
+        'ALEX','CHONGTIAN','DANNY','MARCO','ABIDUR','ANKITA','ANDY','ETHAN','AMANDA',
+        'AIDAN','LINDA','QIANJUN','JIAYING','KISHI'
+        ],
+           5: [ 
+                'ADITYA','MARGIE','RACHEL','ALEXANDER','ZIYAD','DANNY','ENDRIT','CADEN',
+                'VEDANT','SUHANA','KYLE','KEVIN','RAYMOND','CHRISTOPHER','JONATHAN','SASHA',
+                'NAFIYU','TIM','WILL','DANIEL','BENJAMIN','CLAIRE','CHLOE','STELLA','TRACY',
+                'JESSICA','JACKIE','WEN YUAN','YINWEI','TIFFANY','JAYDEN DANIEL','PRINCEDEN' 
+              ]
+         }
+
+def chooseRandomDevo():
+    classrng = random.randint(0,len(krewes))
+    devorng = random.randint(0,len(krewes.get(classrng)))
+    print(classrng)
+    print(devorng)
+chooseRandomDevo()
