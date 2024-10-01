bcf6f0b6f719bec079a0136b13548c680b2db946
Brian Liu <bliu50@stuy.edu>
2024-10-01 14:13:28 -0400

    13

--- 13_tempwork/app.py
+++ 13_tempwork/app.py
@@ -3,27 +3,36 @@
 
 
 from flask import Flask, render_template
-import csv
-import random
-
-
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "No hablo queso!"
 
-with open("data/occupations.csv", newline='') as csvfile:
-    reader = csv.DictReader(csvfile) #reads file
-    percentages = []
-    occupations = []
+# ~~ working code 
+import csv
+import random
+
+with open("data/occupations.csv", newline="") as csvfile:
+    #creates a dictionary for every row that can be parsed through
+    reader = csv.DictReader(csvfile)
+    jobs = []
+    percents = []
+    full = []
     for row in reader:
-        percentages.append(float(row['Percentage']))
-        occupations.append(row['Job Class'])
-        #random choices return a list, [:-1] to remove last line, k = size of outputted list
-        
-def chooseRandomOcc():
-    return random.choices(occupations[:-1], weights = percentages[:-1], k = 1)[0]
-@app.route("/wdywtbwygp")
+        jobs.append(row['Job Class']), percents.append(float(row['Percentage']))
+        full.append({"Job Class": row['Job Class'], "Percentage": row['Percentage']})
+
+def ReturnRandom():
+# random.choices returns a list, [:-1] to ignore last row, k is returned list size
+    return (random.choices(jobs[:-1], weights=percents[:-1], k=1)[0])
+
+# ~~ back to app stuff
+
+@app.route("/wdywtbwygp") 
 def test_tmplt():
-    return render_template( 'tablified.html', foo="OCCUPATIONS", randocc=chooseRandomOcc(), per=percentages,occ=occupations)
+    return render_template( 'tablified.html', foo= ReturnRandom(), coll = full)
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
