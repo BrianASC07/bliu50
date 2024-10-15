7c65546446ff5cd60528221d19d2fdb0f4983fad
Brian Liu <bliu50@stuy.edu>
2024-10-15 11:11:03 -0400

    updated 16 file names and worked on 17

--- 16_flask-sessions/app.py
+++ 16_flask-sessions/app.py
@@ -1,6 +1,7 @@
-# Clyde 'Thluffy' Sinclair
+# Brian Liu
+# 31
 # SoftDev
-# October 2024
+# October 8 2024
 
 # import conventions:
 # list most general first (standard python library)
@@ -10,50 +11,45 @@
 from flask import Flask             #facilitate flask webserving
 from flask import render_template   #facilitate jinja templating
 from flask import request           #facilitate form submission
-from flask import session
+from flask import session, redirect
 import os
 
+
+#the conventional way:
+#from flask import Flask, render_template, request
+
 app = Flask(__name__)    #create Flask object
+
 app.secret_key = os.urandom(32)
 
-@app.route("/", methods=['GET','POST'])
+@app.route("/", methods=['GET', 'POST'])
 def disp_loginpage():
-    #print("\n\n\n")
-    #print("***DIAG: this Flask obj ***")
-    #print(app)
-    #print("***DIAG: request obj ***")
-    #print(request)
-    #print("***DIAG: request.args ***")
-    #print(request.args)
-    #print("***DIAG: request.args['username']  ***")
-    #print(request.args['username'])
-    #print("***DIAG: request.headers ***")
-    #print(request.headers)
-    return render_template( 'login.html' )
+    if 'username' in session:
+        rp = "<p>Logged in as " + session['username'] + "</p>"
+        rp += "<br><form action='/logout'>"
+        rp += "<input type='submit' value='logout?'>"
+        rp += "</form>"
+    else:
+        rp = "Login?"
+        rp += "<h3>Enter your username below to proceed.</h3>"
+        rp += "<form action='/auth'>"
+        rp += "<input type='submit' value='==>'>"
+        rp += "</form>"
+    return render_template( 'login.html', foo = rp )
 
 
-@app.route("/auth", methods=['GET','POST'])
+@app.route("/auth", methods=['GET', 'POST'])
 def authenticate():
-    ###DEBUGGING PRINT STATEMENTS
-    #print("\n\n\n")
-    #print("***DIAG: this Flask obj ***")
-    #print(app)
-    #print("***DIAG: request obj ***")
-    #print(request.form['username'])
-    #print("***DIAG: request.args ***")
-    #print(request.args)
-    #print("***DIAG: request.args['username']  ***")
-    #print(request.args['username'])
-    #print("***DIAG: request.headers ***")
-    #print(request.headers)
-    session['username'] = request.form('username')
-    return render_template('response.html', username = session['username'])  #response to a form submission
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect("http://127.0.0.1:5000/")
+    return render_template( 'response.html' )
 
-
-@app.route("/auth", methods=['GET'])
+@app.route("/logout", methods=['GET'])
 def logout():
-    session.pop('username')
+    session.pop('username', None)
     return redirect("http://127.0.0.1:5000/")
+
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
     app.debug = True
