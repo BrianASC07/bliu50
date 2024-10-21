82730d987ebbd3ed73a0e26f40d22e2d5234f591
Brian Liu <bliu50@stuy.edu>
2024-10-21 11:02:07 -0400

    fixed small error

--- 19_csv2db/build_db.py
+++ 19_csv2db/build_db.py
@@ -5,7 +5,7 @@
 
 #QCC -
 #what is integer primary key? how does it differ from just using int? why can't I have more than one integer primary key
-#when I run from thonny, why do I get an error stating table already exists?
+#when I run from thonny, why do I get an error stating table already exists? --why do i need to delete the database ervytime I make a change?
 #
 
 import sqlite3   #enable control of an sqlite database
@@ -22,19 +22,20 @@ c.execute("CREATE TABLE students (name TEXT, age int, id int)") #creates databas
 with open("students.csv", newline='') as studentscsv:
     reader = csv.DictReader(studentscsv) #reads file
     for row in reader:
-        command = "(" + row["name"] + ", " + str(row["age"]) + ", " + str(row["id"]) + ")"
+        command = "(\"" + row["name"] + "\", \"" + str(row["age"]) + "\", \"" + str(row["id"]) + "\");"
         c.execute("INSERT INTO students VALUES " + command) #adds into the data base the row, age, and id of the students, line by line (probably could use f strings)
-
-c.execute("CREATE TABLE courses (st TEXT, mark int, id int)")#creates database
+c.execute("CREATE TABLE courses (st TEXT, mark int, id int)") #creates database
 with open("courses.csv", newline='') as coursescsv:
     reader = csv.DictReader(coursescsv) #reads file
     for row in reader:
-        command = "(" + row["st"] + ", " + str(row["mark"]) + ", " + str(row["id"]) + ")"
+        command = "(\"" + row["code"] + "\", \"" + str(row["mark"]) + "\", \"" + str(row["id"]) + "\");"
         c.execute("INSERT INTO courses VALUES " + command) #adds into the data base the row, age, and id of the students, line by line (probably could use f strings)
         
 #command = ""          # test SQL stmt in sqlite3 shell, save as string
 #c.execute(command)    # run SQL statement
 
+c.execute("SELECT * FROM students;")
+c.execute("SELECT * FROM courses;")
 #==========================================================
 
 db.commit() #save changes
