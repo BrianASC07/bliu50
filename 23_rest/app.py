80fbf31ac967ec3487d1d492ea26322551b36052
BrianASC07 <brianliu.ny@gmail.com>
2024-11-20 23:05:29 -0500

    formatting changes

--- 23_rest/app.py
+++ 23_rest/app.py
@@ -10,21 +10,20 @@
 
 
 from flask import Flask, render_template
-import json
+import json, os
 import urllib.request
 
 app = Flask(__name__)
+app.secret_key = os.urandom(32)
 
 #gets API key from text file
 API_KEY = open('key_nasa.txt','r').read()
+
 @app.route("/", methods=['GET', 'POST'])
 def main():
-    #returns object using the link with properties url, headers, and status
-    data = urllib.request.urlopen(f"https://api.nasa.gov/planetary/apod?api_key={API_KEY}")
-    #returns source code and converts it into a python dict
-    d = json.loads(data.read())
-    #get values from dict 
-    desc = d.get("explanation")
+    data = urllib.request.urlopen(f"https://api.nasa.gov/planetary/apod?api_key={API_KEY}")     #returns object using the link with properties url, headers, and status
+    d = json.loads(data.read())     #returns source code and converts it into a python dict
+    desc = d.get("explanation")     #get values from dict 
     image = d.get("hdurl")
     return render_template('main.html', desc=desc, image=image)
 
