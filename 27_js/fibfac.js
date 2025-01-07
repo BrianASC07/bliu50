40f0d29a0e2a2d5dd5ffff7b77fef6951a7e2525
BrianASC07 <brianliu.ny@gmail.com>
2025-01-07 10:20:07 -0500

    w27

---
+++ 27_js/fibfac.js
@@ -0,0 +1,22 @@
+/* 
+Tanzeem Hasan, Ethan Sie, Brian Liu
+Chewy_Chupucabras
+SoftDev
+K27: JS is just Scheme with Java syntax
+2025-01-06
+time spent: 10 mins
+*/
+
+function fac(n) {
+    if (n === 0) {
+      return 1;
+    }
+    return n * fac(n - 1);
+  }
+  
+  function fib(n) {
+    if (n <= 1) {
+      return n;
+    }
+    return fib(n - 1) + fib(n - 2);
+  }
---
+++ 27_js/fibfac.scm
@@ -0,0 +1,16 @@
+; Tanzeem Hasan, Ethan Sie, Brian Liu
+; Chewy_Chupucabras
+; SoftDev
+; K27: JS is just Scheme with Java syntax
+; 2025-01-06
+; time spent: 5 mins
+  
+(define (fib n)
+  (if (<= n 1)
+      n
+      (+ (fib (- n 1)) (fib (- n 2)))))
+
+(define (fac n)
+  (if (= n 0)
+      1
+      (* n (fac (- n 1)))))
